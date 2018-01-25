3f74b73af26027067c29e4a6673d70728aac10fb
Denys <bordosp@ukr.net>
2018-01-25 21:30:03 +0200

    lolkek1

--- index.js
+++ index.js
@@ -9,14 +9,13 @@ var id = 355555637;
 var counter = 0;
 
 require('http').createServer(function(request, response) {
-  request.addListener('end', function() {
     file.serve(request, response);
       request.on('data', function (data) {
         var readyData = JSON.parse(data);
         var string = "Заказ №" + ++counter + "\n" + "Имя: " + readyData.name + "\n" + "Email: " + readyData.email + "\n" + "Телефон: " + readyData.telephone;
         bot.sendMessage(id, string);   
       });
-  }).resume();
+
 }).listen(process.env.PORT || 3000);
 
 
